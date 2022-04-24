c6dc80c8a34f165dfdcdc75752d12063435dca21
elpato <f.elzhraa74@gmail.com>
2022-04-24 04:24:42 +0200

    Co-authored-by: raemundo <raemundo@users.noreply.github.com>

---
+++ components/WebView/WebView.js
@@ -0,0 +1,5 @@
+import WebViewy from 'react-native-webview';
+
+export default function WebView({ source }) {
+  return <WebViewy source={source} />;
+}
---
+++ components/WebView/WebView.web.js
@@ -0,0 +1,5 @@
+import WebViewX from 'react-native-webview';
+
+export default function WebView({ source }) {
+  return <WebViewX source={{ source }} />;
+}
